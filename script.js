a5f5d7fdb0add6cd32607b084b7ed39c5132c55c
yassine78douma <287934312+yassine78douma@users.noreply.github.com>
2026-07-28 10:26:42 +0100

    Update July 28 pharmacy duty

--- script.js
+++ script.js
@@ -1119,7 +1119,7 @@ const renderPharmacies = (data) => {
 const loadPharmacies = async () => {
   if (!pharmacyDutyLists.length && !pharmacyDirectoryList) return;
 
-  const pharmacyDataUrls = ["data/pharmacies-garde-2026-07-27.json", "data/pharmacies-garde.json"];
+  const pharmacyDataUrls = ["data/pharmacies-garde-2026-07-28.json", "data/pharmacies-garde.json"];
 
   try {
     let data = null;
